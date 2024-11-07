6543f82fdb0cd3c37a37fc3a7752af59fd4fb7a3
Andrew St. Jean <astjean@symplr.com>
2024-11-07 10:56:02 -0600

    chore: add alloy

--- src/components/LivePreview.tsx
+++ src/components/LivePreview.tsx
@@ -9,17 +9,24 @@ const LivePreview = ({ code }: { code: string }) => {
 
       await new Promise((resolve) => setTimeout(resolve, 100));
 
-      console.log(iframeRef.current.contentDocument);
       const iframeDoc = iframeRef.current.contentDocument;
       if (!iframeDoc) return;
 
       iframeDoc.open();
-      iframeDoc.write(`
+      iframeDoc.write(/*html*/ `
         <!DOCTYPE html>
         <html>
           <head>
             <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
             <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+            <!-- Components for modern browsers -->
+            <script type="module" src="/node_modules/@symplr-ux/alloy-components/dist/symplr-stencil-components/symplr-stencil-components.esm.js"></script>
+            <!-- Iconography -->
+            <link rel="stylesheet" type="text/css" href="/node_modules/@symplr-ux//alloy-icons/dist/icons.min.css">
+            <!-- Font -->
+            <link rel="stylesheet" type="text/css" href="/node_modules/@symplr-ux/alloy-theme/dist/fonts/lato.min.css">
+            <!-- Theme -->
+            <link rel="stylesheet" type="text/css" href="/node_modules/@symplr-ux/alloy-theme/dist/css/sympl-theme.min.css">
           </head>
           <body>
             <div id="root">
